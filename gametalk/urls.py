87fca0e5a3d6a337609eb6d6b02994c07e74a5b8
ryan-a-davis365 <itsryzahd@gmail.com>
2025-02-15 17:50:11 +0000

    fix: sorted out duplicated folders

--- gametalk/urls.py
+++ gametalk/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home import views as home_views
 
 urlpatterns = [
+    path("home/", home_views.home_page, name='home'),
     path('admin/', admin.site.urls),
 ]
