4d9f3c00f14c990f1b20ffa33ed409054ac4b280
ryan-a-davis365 <itsryzahd@gmail.com>
2025-02-16 15:42:11 +0000

    feat: setup secret key for deployment

--- gametalk/settings.py
+++ gametalk/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c@r=@of!=b)r98oxc0%7ls(-iggz3%nlwj9dcm3%aw&o@$ww5c'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
