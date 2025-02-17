62f026fec6682513bfd2819f337c650efb59c632
ryan-a-davis365 <itsryzahd@gmail.com>
2025-02-17 21:15:53 +0000

    feat: added "class Post" to models.py

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
+    platform = models.TextField()
+    release_date = models.TextField()
+    genre = models.TextField()
+    publisher = models.CharField()
+    developer = models.CharField()
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="game_posts"
+    )
