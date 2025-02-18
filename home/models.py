9e0e0bde70adeab01ad70142e1717c87bf4b1fd0
ryan-a-davis365 <itsryzahd@gmail.com>
2025-02-18 13:47:44 +0000

    feat: added to comment and post class so both display time created and who created them

--- home/models.py
+++ home/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
     platform = models.CharField()
     release_date = models.CharField()
     genre = models.CharField()
@@ -15,6 +16,12 @@ class Post(models.Model):
     )
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.created_by}"
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -24,3 +31,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.created_by}"
