16336737bb9cce27ba221bb6b00fe6fd535aaceb
ryan-a-davis365 <itsryzahd@gmail.com>
2025-02-18 15:01:56 +0000

    feat: created home_page.html and and urls.py in the home directory

--- home/views.py
+++ home/views.py
@@ -1,7 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
-
-def home_page(request):
-    return HttpResponse("Hello, this is my home page!")
