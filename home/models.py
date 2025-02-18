9ce2a3306f30b362b3fa55d048f84f8dc7a55cf0
ryan-a-davis365 <itsryzahd@gmail.com>
2025-02-18 11:33:41 +0000

    feat: updated admin.py so "class Post" appears on the admin page

--- home/models.py
+++ home/models.py
@@ -5,11 +5,12 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     description = models.TextField()
-    platform = models.TextField()
-    release_date = models.TextField()
-    genre = models.TextField()
+    platform = models.CharField()
+    release_date = models.CharField()
+    genre = models.CharField()
     publisher = models.CharField()
     developer = models.CharField()
     created_by = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="game_posts"
     )
+    updated_on = models.DateTimeField(auto_now=True)
