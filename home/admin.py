432d7a5dab1fa158b60876dd5f2bc77116c08c4c
ryan-a-davis365 <itsryzahd@gmail.com>
2025-02-19 11:53:36 +0000

    feat: added summernote to admin.py so you can view drafts by date created in the admin panel

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    
+    list_display = ('title', 'created_on', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'description': ('title',)}
+    summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
