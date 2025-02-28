eefd247d1f2425e6acca9d88ee08dc81c60ddf69
ryan-a-davis365 <itsryzahd@gmail.com>
2025-02-28 13:49:55 +0000

    feat: added search bar and styled it with css

--- home/views.py
+++ home/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
+from django.db.models import Q
 
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1)
     template_name = "home/index.html"
     paginate_by = 6
     context_object_name = 'post_list'
+
+    def get_queryset(self):
+        queryset = Post.objects.filter(status=1)
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(title__icontains=query) |
+                Q(release_date__icontains=query) |
+                Q(platform__icontains=query) |
+                Q(genre__icontains=query)
+            )
+        return queryset
 
 def post_detail(request, slug):
     queryset = Post.objects.filter(status=1)
