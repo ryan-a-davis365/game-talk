86dd9a5fa6fc444b3885e98f3e24213126964587
ryan-a-davis365 <itsryzahd@gmail.com>
2025-07-08 11:15:41 +0100

    fic: removed commented out code

--- gametalk/settings.py
+++ gametalk/settings.py
@@ -100,13 +100,6 @@ WSGI_APPLICATION = 'gametalk.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
